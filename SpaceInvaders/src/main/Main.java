f2172c3e5e8094f693f787a5ebb1e07fa8c390cf
StevenMallqui <stevenma1399@gmail.com>
2019-12-11 18:26:57 +0100

    OLD MAIN

--- SpaceInvaders/src/main/Main.java
+++ SpaceInvaders/src/main/Main.java
@@ -12,7 +12,61 @@ import game.Level;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+
+public class Main{
+	
+	public static void main(String[] args) {
+		
+		 long seed = 0l;
+		 String level;
+		
+	 if (args.length > 0) {
+			 
+			 // Read level
+			 level = args[0].toUpperCase();
+			 System.out.println("Level : " + level);
+			 
+			 
+			 // Read seed
+			 if(args.length == 2) {
+				 try {
+					 seed = Integer.parseInt(args[1]);
+				 }
+				 
+				 catch(NumberFormatException e) {
+					 System.out.println("   Loading random seed ...");
+					 seed = new Random(System.currentTimeMillis()).nextInt();  
+				 }
+			 }
+			 
+			 Random rand = new Random(seed);
+			 Level nvl = null;
+			 
+				if (level.equals("EASY"))
+					nvl = Level.EASY;
+					
+				else if (level.equals("HARD"))
+					nvl = Level.HARD;
+					
+				else if (level.equals("INSANE"))
+					nvl = Level.INSANE;
+			
+				 Game game = new Game(nvl, rand);
+				 Controller controlador = new Controller(game);
+				 
+				 // Run game
+				 controlador.run();
+	 	}
+		
+	 else 
+		 System.err.println("   No level detected ... ");
+		 
+	}
+	
+}
+
+
+/* public class Main {
 	
 	// Static main class
 	 public static void main(String[] args) {
@@ -25,7 +79,7 @@ public class Main {
 		 
 				 while (!entry) {
 					 
-					 try {
+					try {
 					 
 					System.out.print("   Introduce Level and/or seed : ");
 				 	Scanner scan = new Scanner(System.in);
@@ -47,12 +101,12 @@ public class Main {
 							    }
 						 }
 					 }
-					 
+					
 					 catch(levelException|NumberFormatException e) {
 						 System.out.println(e.getMessage());
 					 }
-				  
-				  } 			 
+				  } 
+				 
 		 Random rand = new Random(seed);
 		 Level nvl = null;
 		 
@@ -75,7 +129,7 @@ public class Main {
 	 
 
 	 
-}
+} */
 	 
 	 
 	 
